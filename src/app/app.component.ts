fce05e68c081bab808dbe42244829aa715b0de56
julian beltran <bequejulian@gmailcomcls>
2021-09-09 23:37:07 -0500

    comunicacion entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primerPrograma';
+  public title: string = 'Primer programa Angular';
+  numero1: number;
+  numero2: number;
+  resultado: number;
+
+  //var let const
+  public operar(op: string): void{
+     switch(op){
+           case 's':
+             this.resultado = this.numero1 + this.numero2;
+             break;
+           case 'r':
+              this.resultado = this.numero1 - this.numero2;   
+              break; 
+           case 'm':
+              this.resultado = this.numero1 * this.numero2;
+              break;
+           case 'd':
+              this.resultado = this.numero1 / this.numero2;    
+              break;   
+            default:
+              this.resultado = 0;                 
+      }
+  }
 }
